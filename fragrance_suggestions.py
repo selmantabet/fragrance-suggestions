4668c4e784aca953e3a506891f2e77963ba178e8
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2021-03-22 00:53:49 +0300

    Initial commit.

---
+++ fragrance_suggestions.py
@@ -0,0 +1,7 @@
+from pandas import*
+#Project: Fragrance Suggestion Engine by Selman Tabet @ https://selman.io/
+#This project is dedicated to the entire Fragrance Community in celebration of 
+#Fragrance Day 2021, as well as in memory of Carlos Powell of BFL.
+
+#The engine is primitive at its current state, as the implementation
+#merely involves a simple tag-matching approach, feel free to help with this project.
